cb6ee777c7095c3be95f4e4e124bea9b90ba620e
feliperx <feliipe.xaud@gmail.com>
2021-03-06 15:41:51 -0400

    testing deploy connection

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -23,7 +23,7 @@ export function CountdownProvider({children} : CountdownProviderProps) {
 
     const {startNewChallenge} = useContext(ChallengeContext);
 
-    const [time, setTime] = useState(30 * 60);
+    const [time, setTime] = useState(20 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
   
@@ -37,7 +37,7 @@ export function CountdownProvider({children} : CountdownProviderProps) {
       function resetCountdown() {
         setIsActive(false);
         clearTimeout(countdownTimeout);
-        setTime(30 * 60);
+        setTime(20 * 60);
         setHasFinished(false);
       }
     
